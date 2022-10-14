51e466dad22e8c13a6f349ea06b5652e51739c4e
ayomide <folorunshoayomide46@gmail.com>
2022-10-14 13:06:42 +0100

    Error fix

--- app.py
+++ app.py
@@ -4,19 +4,8 @@ import os
 from collections import Mapping
 
 app = Flask(__name__)
-CORS(app)
 
 
-
-@app.after_request
-def after_request(response):
-    response.headers.add("Access-Control-Allow-Headers", 'Content-Type, Authorization')
-    response.headers.add("Access-Control-Allow-Headers", "GET,POST,DELETE,PATCH,OPTIONS")
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response 
 
 @app.route("/")
 def index():
--- static/script.js
+++ static/script.js
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const search_btn = document.getElementById("search-btn");
-  const meal_List = document.getElementById("meal");
-  const meal_details_content = document.querySelector(".meal-details-content");
-  const recipe_close = document.getElementById("recipe-close-btn");
+document.addEventListener('DOMContentLoaded', function () {
+  const search_btn = document.getElementById('search-btn')
+  const meal_List = document.getElementById('meal')
+  const meal_details_content = document.querySelector('.meal-details-content')
+  const recipe_close = document.getElementById('recipe-close-btn')
 
   // event listener
-  search_btn.addEventListener("click", get_meals);
-  meal_List.addEventListener("click", get_recipe);
-  recipe_close.addEventListener("click", () => {
-    meal_details_content.parentElement.classList.remove("showRecipe");
-  });
+  search_btn.addEventListener('click', get_meals)
+  meal_List.addEventListener('click', get_recipe)
+  recipe_close.addEventListener('click', () => {
+    meal_details_content.parentElement.classList.remove('showRecipe')
+  })
 
   function get_meals() {
-    let search = document.getElementById("search-input").value.trim();
-    console.log(search);
+    let search = document.getElementById('search-input').value.trim()
+    console.log(search)
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`)
       .then((response) => response.json())
       .then((data) => {
-        let html = "";
-        console.log(data.meals);
+        let html = ''
+        console.log(data.meals)
         if (data.meals) {
           data.meals.forEach((meal) => {
             html += `
@@ -31,30 +31,30 @@ document.addEventListener("DOMContentLoaded", function () {
                                 <a href="#" class="recipe-btn">Get Recipe</a>
                             </div>
                         </div>
-                    `;
-          });
-          meal_List.classList.remove("notFound");
+                    `
+          })
+          meal_List.classList.remove('notFound')
         } else {
-          meal_List.classList.add("notFound");
-          html = "Sorry, we couldn't find a meal.";
+          meal_List.classList.add('notFound')
+          html = "Sorry, we couldn't find a meal."
         }
-        meal_List.innerHTML = html;
-      });
+        meal_List.innerHTML = html
+      })
   }
 
   function get_recipe(e) {
-    e.preventDefault;
-    console.log(e.target);
-    if (e.target.classList.contains("recipe-btn")) {
-      let mealiteam = e.target.parentElement.parentElement;
+    e.preventDefault
+    console.log(e.target)
+    if (e.target.classList.contains('recipe-btn')) {
+      let mealiteam = e.target.parentElement.parentElement
       fetch(
         `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealiteam.dataset.id}`
       )
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
-          meal = data.meals[0];
-          console.log(meal);
+          console.log(data)
+          meal = data.meals[0]
+          console.log(meal)
           let html = `
                     <div class="meal-details-content">
                         <h2 class = "recipe-title">${meal.strMeal}</h2>
@@ -70,10 +70,10 @@ document.addEventListener("DOMContentLoaded", function () {
                             <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
                         </div>
                     </div>
-                `;
-          meal_details_content.innerHTML = html;
-          meal_details_content.parentElement.classList.add("showRecipe");
-        });
+                `
+          meal_details_content.innerHTML = html
+          meal_details_content.parentElement.classList.add('showRecipe')
+        })
     }
   }
-});
+})
